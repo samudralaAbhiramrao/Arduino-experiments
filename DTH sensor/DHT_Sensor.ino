a1ae565a6f51bace58cb068deb0e9c0c9eb7a346
Samudrala Abhiramrao <67550743+samudralaAbhiramrao@users.noreply.github.com>
2020-12-17 19:42:35 +0530

    Create DHT_Sensor.ino

---
+++ DTH sensor/DHT_Sensor.ino
@@ -0,0 +1,27 @@
+#include<dht.h>
+dht dht11;
+
+// define the DHT pin
+const int dht_pin = 5;
+
+void setup() {
+  Serial.begin(9600); 
+}
+
+void loop() { 
+  
+  dht11.read11(dht_pin); 
+  Serial.print("Humidity = "); 
+  Serial.println(dht11.humidity); 
+
+  //Temparature in Celsius
+  Serial.print("Temparature in Celsius = "); 
+  Serial.println(dht11.temperature);
+
+  //Temparature in Fahrenheit
+  //T(°F) = T(°C) × 9/5 + 32
+  Serial.print("Temparature in Fahrenheit = ");
+  Serial.println((dht11.temperature * 9/5) + 32);
+  
+  delay(2000);
+}
