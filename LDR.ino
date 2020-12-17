a365038585efddbe2a489ec496eac19f7db6a75c
Samudrala Abhiramrao <67550743+samudralaAbhiramrao@users.noreply.github.com>
2020-12-17 07:54:07 +0530

    Add files via upload

---
+++ LDR.ino
@@ -0,0 +1,24 @@
+//Define pins: 13 for LED and A0 for LDR
+const int led = 13;
+const int ldr = A0;
+
+void setup () {
+  pinMode(led, OUTPUT); //set led pin as output
+  pinMode(ldr, INPUT);  //set ldr pin as input
+}
+
+void loop() {
+  //Declare an integer variable ldrValue
+  //read signal from ldr pin and store reading in ldrValue
+  int ldrValue = analogRead(ldr);
+
+  //reading range from 0 to 1023
+  //if reading is less than 500 (DARK) turn on LED
+  if (ldrValue <= 500) {
+  digitalWrite(led, HIGH); 
+  }
+  //else (there is enough light in the room) LED is off
+  else {  
+    digitalWrite(led, LOW);
+  }
+}
---
+++ water_Sensor.ino
@@ -0,0 +1,34 @@
+//Define pins
+const int s_pin = A0; //S pin on water sensor connected to AO pin on Arduino board
+const int led = 13; //LED connected to pin 13 on the Arduino board
+
+void setup()
+{
+  Serial.begin(9600);   //Begin serial communication
+  pinMode(s_pin, INPUT);   //set s_pin as input
+  pinMode(led, OUTPUT);   //ser led as output
+}
+
+void loop()
+{
+  Serial.println(analogRead(s_pin)); //send s_pin reading to serial monitor
+  delay(100);  //delay 100 ms
+
+  int value = analogRead(s_pin); //Read data from analog pin and store it to value variable
+  if (value > 10 && value <= 400 ){ 
+    Serial.println("Water reached the bottom of the sensor!");
+    digitalWrite(led, HIGH);
+  }
+  else if (value > 400 && value <= 650 ){ 
+    Serial.println("Water reached middle section of the sensor!");
+    digitalWrite(led, HIGH);
+  }
+  else if (value > 650 ){ 
+    Serial.println("Water reached top section of the sensor!");
+    digitalWrite(led, HIGH);
+  }
+  else {
+    Serial.println("Water not touching the sensor!");
+    analogWrite(led, LOW); 
+  }
+}
