82caf0b6125756f97ad9c8f259a74df8e3883875
Samudrala Abhiramrao <67550743+samudralaAbhiramrao@users.noreply.github.com>
2020-12-17 19:49:28 +0530

    Touch Sensor code

---
+++ Touch_Sensor/Touch_Sensor.ino
@@ -0,0 +1,19 @@
+//Define pins
+const int sig_pin = 2; //S pin on water sensor connected to AO pin on Arduino board
+const int led = 13; //LED connected to pin 13 on the Arduino board
+
+void setup()
+{
+  pinMode(sig_pin, INPUT);   //set sig_pin as input
+  pinMode(led, OUTPUT);   //ser led as output
+}
+
+void loop()
+{
+  if (digitalRead(sig_pin)){ 
+    digitalWrite(led, HIGH);
+  }
+  else {
+    digitalWrite(led, LOW); 
+  }
+}
